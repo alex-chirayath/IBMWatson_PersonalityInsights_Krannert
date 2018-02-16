02cd9da512d5bf73ba84045ae1b4190eefb57f19
Julianna Stevenson <steve146@purdue.edu>
2018-02-15 23:54:51 -0500

    get words spoken by interviewee

--- phase2/speech_analysis.py
+++ phase2/speech_analysis.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pprint
+import sys
 
 url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
 username = "be828bbc-2a8d-47e5-838b-ab79102d06c6"
@@ -44,7 +46,7 @@ def getjson(filename):
     Args:
         filename: String name of mp3 file, including the path to that file
     Returns:
-        json: json object containing audio transcription and speaker metadata
+        A json object containing audio transcription and speaker metadata
     """
 
     headers = {"Content-Type" : "audio/mp3"}
@@ -55,8 +57,59 @@ def getjson(filename):
 
     return r.json()
 
+def pick_interviewee(speaker):
+    """ Choose which speaker is interviewee.
+
+    Args:
+        speaker: dictionary of speaker id to words spoken by that speaker
+    Returns:
+        An integer speaker id of person identified as interviewee
+    """
+    max_words = 0
+    interviewee = -1
+
+    for key, value in speaker.items():
+        if len(value) > max_words:
+            max_words = len(value)
+            interviewee = key
+
+    return interviewee
+
+def get_interviewee_words(speaker_data):
+    """ Determine words spoken by interviewee.
+
+    Args:
+        speaker_data: json object containing mp3 metadata
+    Returns:
+        A list of strings that are the words spoken by the interviewee
+    """
+    speakers = {}
+    interviewee = -1
+    speaker_labels = list(speaker_data['speaker_labels'])
+
+    for item in speaker_data['results']:
+        for words in item['alternatives']:
+            for word in words['timestamps']:
+                for label in speaker_labels:
+                    if word[1] == label['from']:
+                        if label['speaker'] in speakers.keys():
+                            speakers[label['speaker']].append(word[0])
+                        else:
+                            speakers[label['speaker']] = [word[0]]
+                        break
+
+    # TODO: change to checking for interviewee halfway through audio
+    interviewee = pick_interviewee(speakers)
+
+    return speakers[interviewee]
+
 def speechanalysis(filename):
     speaker_data = getjson(filename)
+
+    json_data = open("data.txt").read()
+    speaker_data = json.loads(json_data)
+
+    interviewee_words = get_interviewee_words(speaker_data)
 
 if __name__ == "__main__":
     speechanalysis(sys.argv[1])
