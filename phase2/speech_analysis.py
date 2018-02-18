57a5b60ba79fd6fbd52c187082e5f2bb8af3eadb
Julianna Stevenson <steve146@purdue.edu>
2018-02-17 22:04:04 -0500

    save personality insights to csv

--- phase2/speech_analysis.py
+++ phase2/speech_analysis.py
@@ -4,22 +4,22 @@ import pprint
 import csv
 import sys
 
-speech_to_text_url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
-speech_to_text_username = "be828bbc-2a8d-47e5-838b-ab79102d06c6"
-speech_to_text_password = "R5gsSmhsiIPv"
+speech_to_text_url = 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize'
+speech_to_text_username = 'be828bbc-2a8d-47e5-838b-ab79102d06c6'
+speech_to_text_password = 'R5gsSmhsiIPv'
 
-insights_url = "https://gateway.watsonplatform.net/personality-insights/api/v2/profile"
-insights_username = "366bd94d-6871-482c-9ca9-84de1d1f0e6a"
-insights_password = "NlgUu6FjF7XF"
+insights_url = 'https://gateway.watsonplatform.net/personality-insights/api/v2/profile'
+insights_username = '366bd94d-6871-482c-9ca9-84de1d1f0e6a'
+insights_password = 'NlgUu6FjF7XF'
 
-def getjson(filename):
+def getspeech(filename):
     """ Translate mp3 to json object with IBM Watson API.
 
     This module takes the name of a mp3 file, including the path to
     that file, and calls the IBM Watson speech-to-text api to
     get a json object describing the audio.
 
-    Example json for audio "Hello world":
+    Example json for audio 'Hello world':
     {'result_index': 0,
         'results': [
             {
@@ -54,9 +54,9 @@ def getjson(filename):
         A json object containing audio transcription and speaker metadata
     """
 
-    headers = {"Content-Type" : "audio/mp3"}
-    audio_file = open(filename, "rb")
-    params = {"model" : "en-US_NarrowbandModel", "speaker_labels" : "true"}
+    headers = {'Content-Type' : 'audio/mp3'}
+    audio_file = open(filename, 'rb')
+    params = {'model' : 'en-US_NarrowbandModel', 'speaker_labels' : 'true'}
 
     r = requests.post(speech_to_text_url, auth=(speech_to_text_username, speech_to_text_password), data=audio_file, headers=headers, params=params)
 
@@ -88,11 +88,14 @@ def getintervieweewords(speaker_data):
     Returns:
         A list of strings that are the words spoken by the interviewee
     """
+    # Key is speakerid, value is list of words associated with each speakerid
     speakers = {}
+
     interviewee = -1
     halfway_timestamp = speaker_data['speaker_labels'][len(speaker_data['speaker_labels']) - 1]['to']/2
     speaker_labels = list(speaker_data['speaker_labels'])
 
+    # Separate speaker's words and add to speakers
     for item in speaker_data['results']:
         for words in item['alternatives']:
             for word in words['timestamps']:
@@ -111,40 +114,97 @@ def getintervieweewords(speaker_data):
     return speakers[interviewee]
 
 def writelisttotxt(filename, list):
-    text_file = open(filename, "w")
+    text_file = open(filename, 'w')
 
     for item in list:
-        text_file.write("%s\n" % item)
+        text_file.write('%s\n' % item)
 
     text_file.close()
 
 def getpersonalityinsights(filename):
-    headers = {"Content-Type" : "text/plain"}
+    """ Get IBM Watson Personality Insights for file.
 
-    print(filename)
+        Args:
+            filename: string name of file to get personality insights from
+        Returns:
+            Response from IBM Watson Personality Insights API.
+    """
+    headers = {'Content-Type' : 'text/plain'}
 
     with open(filename, 'r') as myfile:
             data=myfile.read()
 
     r = requests.post(insights_url, auth=(insights_username, insights_password), data=data, headers=headers)
 
-    print(r.json())
+    return r
 
-    return r.json()
+def headeradder():
+    return ["id","Adventurousness","Artistic interests","Emotionality","Imagination","Intellect","Authority-challenging","Achievement striving",
+            "Cautiousness","Dutifulness","Orderliness","Self-discipline","Self-efficacy","Activity level","Assertiveness","Cheerfulness",
+            "Excitement-seeking","Outgoing","Gregariousness","Altruism","Cooperation","Modesty","Uncompromising","Sympathy","Trust","Fiery",
+            "Prone to worry","Melancholy","Immoderation","Self-consciousness","Susceptible to stress","Challenge","Closeness","Curiosity",
+            "Excitement","Harmony","Ideal","Liberty","Love","Practicality","Self-expression","Stability","Structure","Conservation",
+            "Openness to change","Hedonism","Self-enhancement","Self-transcendence"]
+
+def jsontocsvwriter(filename,data):
+    """ Save Personality Insights Profile to 'values.csv'.
+
+        Args:
+            filename: string identifying file insights are being saved for
+            data: response from IBM Watson Personality Profile API
+        Returns:
+            void
+    """
+    csvfile = open(filename, 'w')
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(headeradder())
+
+    # Write Personality Insights data to csv
+    csvdata = []
+    csvdata.append(filename.replace('.cvs', ''))
+    v1 =  data['tree']
+    headers = []
+    for i in v1:
+        if i == 'children':
+            for j in v1[i]:
+                for k in j:
+                    if k == 'children':
+                        vals = []
+                        for l in j[k]:
+                            for m in l['children']:
+                                if 'children' in m:
+                                    for n in m['children']:
+                                        csvdata.append(n['percentage'])
+                                else:
+                                    csvdata.append(m['percentage'])
+    csvwriter.writerow(csvdata)
 
 def speechanalysis(filename):
-    speaker_data = getjson(filename)
+    """ Get Personality Profile from interview.
 
-    json_data = open("data.txt").read()
-    speaker_data = json.loads(json_data)
+        Args:
+            filename: string name of mp3 file to analyze
+        Returns:
+            void
+    """
+    # Get json response from speech-to-text API
+    speaker_data = getspeech(filename)
 
+    # Get list of words associated with interviewee
     interviewee_words = getintervieweewords(speaker_data)
 
-    text_file_name = filename.replace(".mp3", "")  + ".txt"
+    text_file_name = filename.replace('.mp3', '')  + '.txt'
 
+    # Write interviewee's words to '<filename>.txt'
     writelisttotxt(text_file_name, interviewee_words)
 
+    # Get response from Personality Insights API
     insights = getpersonalityinsights(text_file_name)
 
-if __name__ == "__main__":
+    csv_file_name = filename.replace('.mp3', '') + '.csv'
+
+    # Write Personality Insights response to '<filename>.csv'
+    jsontocsvwriter(csv_file_name, json.loads(insights.text))
+
+if __name__ == '__main__':
     speechanalysis(sys.argv[1])
